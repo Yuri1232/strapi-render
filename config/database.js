9e92a949f948a185aea15f48fca7284aae5f2ba0
Younus Kawa Mohammed <younus.k@codetalkers.eu>
2023-09-06 21:05:57 +0300

    database config

--- config/database.js
+++ config/database.js
@@ -55,7 +55,6 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"),
         host: env(
           "DATABASE_HOST",
           "dpg-cjsa8sdv2qks739n02k0-a.frankfurt-postgres.render.com"
@@ -64,22 +63,7 @@ module.exports = ({ env }) => {
         database: env("DATABASE_NAME", "strapidb_d43w"),
         user: env("DATABASE_USERNAME", "admin"),
         password: env("DATABASE_PASSWORD", "KjqCA6lPnZCmeOyeUAm1H7ZDsQ4TGCfz"),
-        ssl: env.bool("DATABASE_SSL", true) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
     sqlite: {
